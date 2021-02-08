5bb52d33cf8515b635ba0f4f908bce0738fa8909
Hammad90 <moh.hammad.90@gmail.com>
2021-02-08 22:11:28 +0400

    updating info box

--- ticketing/client/pages/tickets/[ticketId].js
+++ ticketing/client/pages/tickets/[ticketId].js
@@ -16,9 +16,10 @@ const TicketShow = ({ ticket }) => {
             <h4>Price: {ticket.price}</h4>
             {errors}
             <button onClick={() => doRequest()} className="btn btn-primary">Purchase</button>
-            <br/>
+            <div className="container">
             <div className="alert alert-info"><h5>To complete the purchase you can try 
                 this credit card "4242 4242 4242 4242" with any dates and cvv</h5></div>
+                </div>
         </div>
     );
 };
